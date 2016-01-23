396681141bce5776b810189f2ee5b3c5511d0d13
Lion Kuo <lionsheart56@gmail.com>
2016-01-23 16:00:10 +0800

    Initial

---
+++ src/ems/MultipleHome/MultiScheduler.java
@@ -0,0 +1,27 @@
+package ems.MultipleHome;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Set;
+import java.lang.Math;
+
+import ems.datastructure.ActivityNode;
+import ems.datastructure.Environment;
+import ems.singleHome.HybridScheduler;
+
+/**
+ * Created by LionKuo on 2016/1/23.
+ */
+public class MultiScheduler {
+
+    public static final int TIME_SLOTS = 24;
+
+    public static void main(String[] args){
+
+        //MultipleScheduler instance = new MultipleScheduler(args);
+        System.out.println(args.length);
+        //instance.run();
+    }
+}
---
+++ src/ems/MultipleHome/MultipleScheduler.java
@@ -0,0 +1,35 @@
+package ems.MultipleHome;
+
+/**
+ * Created by LionKuo on 2016/1/23.
+ */
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Set;
+import java.lang.Math;
+
+import ems.datastructure.ActivityNode;
+import ems.datastructure.Environment;
+import ems.singleHome.HybridScheduler;
+
+
+public class MultipleScheduler {
+    public static ArrayList<Environment> allHome = new ArrayList<Environment>();
+    private int numOfHome = 0;
+
+    public MultipleScheduler(String[] args){
+        this.numOfHome = args.length;
+    }
+
+
+
+    // The entrance of all program.
+    public void run(){
+
+    }
+
+}
